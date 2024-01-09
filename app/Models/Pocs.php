83a7113a3e78d4a12a94f7bbdb76b9ed2816c971
MichelCode <michelseocode.com>
2024-01-09 16:15:10 +0100

    v6

--- app/Models/Pocs.php
+++ app/Models/Pocs.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Pocs extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'conclusion',
+        'description',
+        'scope_vulnerability_id',
+        'ordre',
+        // Add any other fillable attributes as needed
+    ];
+
+    // attention probablement faux
+    public function scopeVulnerability()
+    {
+        return $this->belongsTo(ScopeVulnerability::class);
+    }
 }
