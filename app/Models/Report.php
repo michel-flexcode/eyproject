b53ab3c02023cece8fcd6a592a9efcba25188fc9
MichelCode <michelseocode.com>
2024-01-09 16:11:04 +0100

    factroyv1

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -9,6 +9,17 @@ class Report extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name_doc',
+        'vulnerabilities',
+        'state',
+        'date',
+        'recommendations',
+        'proposals',
+        'conclusions',
+        // Ajoutez d'autres attributs remplissables au besoin
+    ];
+
     public function company()
     {
         return $this->belongsTo(Company::class);
