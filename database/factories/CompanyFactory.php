f8e4be24997d11dff6e6730d82b752f7fb737733
MichelCode <michelseocode.com>
2024-01-09 16:20:02 +0100

    factory compagny

--- database/factories/CompanyFactory.php
+++ database/factories/CompanyFactory.php
@@ -2,13 +2,18 @@
 
 namespace Database\Factories;
 
+use App\Models\Company;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
-/**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Company>
- */
 class CompanyFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Company::class;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +22,11 @@ class CompanyFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'name' => $this->faker->company,
+            'web' => $this->faker->url,
+            'mail_domain' => $this->faker->domainName,
+            'logo' => $this->faker->imageUrl(),
+            // Add any other attributes as needed for the Company model
         ];
     }
 }
